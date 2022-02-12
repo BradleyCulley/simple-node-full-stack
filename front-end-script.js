998a9d5042b7f6a79b2c1770712cc81940632f11
Brad Culley <culleyib@gmail.com>
2022-02-12 00:05:33 -0600

    initial commit

---
+++ front-end-script.js
@@ -0,0 +1,25 @@
+fetch("http://localhost:8080/api")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        displayData(data);
+    });
+
+
+function displayData(data) {
+    const outputArea = document.getElementById('rootContainer');
+
+    const output = data.reduce((accumulator, stockEntry) => {
+        return `${accumulator} 
+                    <h3>Entry:</h3>
+                    <div>Date: ${stockEntry.date}</div>
+                    <div>Open: ${stockEntry.open}</div>
+                    <div>High: ${stockEntry.high}</div>
+                    <div>Low: ${stockEntry.low}</div>
+                    <div>Close: ${stockEntry.close}</div>
+                    <div>Volume: ${stockEntry.volume}</div>
+                `;
+    }, '');
+
+    outputArea.innerHTML = output;
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const request = require('request');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.get('/api', (req, res) => {
+  const requestOptions = {
+    'url': 'https://api.tiingo.com/tiingo/daily/aapl/prices?startDate=2019-01-02&token=7f152d077759157d903064be4bc4b6c2fab11ddf',
+    'headers': {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  request(requestOptions, (error, response, body) => res.send(body));
+});
+
+app.use(express.static(__dirname));
+
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port);
+console.log("server started on port " + port);
