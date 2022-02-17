c84106fc83903be2aa9761e3e6fce658757db85d
matthewparks05 <matthewparks05@gmail.com>
2022-02-16 22:07:10 -0500

    adding sum

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
-const express = require('express');
-const path = require('path');
-const request = require('request');
+const express = require("express");
+const path = require("path");
+const request = require("request");
 const port = process.env.PORT || 80;
 const app = express();
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   const requestOptions = {
-    'url': 'https://api.tiingo.com/tiingo/daily/aapl/prices?startDate=2019-01-02&token=7f152d077759157d903064be4bc4b6c2fab11ddf',
-    'headers': {
-      'Content-Type': 'application/json'
-    }
+    url: "https://api.tiingo.com/tiingo/daily/aapl/prices?startDate=2019-01-02&token=7f152d077759157d903064be4bc4b6c2fab11ddf",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  request(requestOptions, (error, response, body) => res.send(body));
+});
+
+app.get("/average", (req, res) => {
+  const requestOptions = {
+    url: "https://api.tiingo.com/tiingo/daily/aapl/prices?startDate=2019-01-02&token=7f152d077759157d903064be4bc4b6c2fab11ddf",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  request(requestOptions, (error, response, body) => {
+    const sum = body.reduce(
+      (accumulator, entry) => accumulator + entry.adjClose,
+      0
+    );
+    res.send({
+      sum,
+    });
+  });
+});
+
+app.get("/add", (req, res) => {
+  const requestOptions = {
+    url: "https://api.tiingo.com/tiingo/daily/aapl/prices?startDate=2019-01-02&token=7f152d077759157d903064be4bc4b6c2fab11ddf",
+    headers: {
+      "Content-Type": "application/json",
+    },
   };
 
   request(requestOptions, (error, response, body) => res.send(body));
@@ -17,8 +47,8 @@ app.get('/api', (req, res) => {
 
 app.use(express.static(__dirname));
 
-app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, 'index.html'));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(port);
